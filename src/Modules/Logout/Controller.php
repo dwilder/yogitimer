a6b7ef1dbf0a8a1266dad7cbf27516420d69fc1e
David Wilder <davidedwardwilder@gmail.com>
2015-05-25 00:41:32 -0400

    Logout functionality works

--- src/Modules/Logout/Controller.php
+++ src/Modules/Logout/Controller.php
@@ -1,30 +1,44 @@
 <?php
 namespace Src\Modules\Logout;
 
-use Src\Includes\SuperClasses\UIController;
-use Src\Modules\Logout\Models\LogoutModel;
+use Src\Includes\SuperClasses\AbstractController;
+use Src\Includes\User\User;
+use Src\Includes\Session\Session;
 
 /*
  * Controller class for logging out.
  */
 
-class Controller extends UIController
+class Controller extends AbstractController
 {
     /*
      * Run the program
      */
-    public function request()
+    public function run()
     {
-        $this->model = new LogoutModel;
-        $this->model->logout();
+        $this->logout();
+    }
+
+    /*
+     * Log out the current user
+     */
+    public function logout()
+    {
+        $user = User::getInstance();
+        
+        if ( $user->isSignedIn() ) {
+            $this->destroySession();
+        }
+        
+        $this->redirect('login');
     }
     
     /*
-     * Implement setModuleName
+     * Destroy the session
      */
-    protected function setModuleName()
+    protected function destroySession()
     {
-        $this->module_name = 'Logout';
+        $session = Session::getInstance();
+        $session->destroy();
     }
-    
 }
